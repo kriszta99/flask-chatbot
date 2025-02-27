4299f394e8018b0b14999423f63abd2e0ce2b815
kriszta99 <lukacs.krisztina@student.ms.sapientia.ro>
2025-02-27 14:19:25 +0200

    I corrected in style.css file the symbol

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -12,7 +12,7 @@ const createChatLi = (message, className) => {
     //Create a chat <li> element with passed message and className
     const chatLi = document.createElement("li");
     chatLi.classList.add("chat", `${className}`);
-    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material symbols-outlined">smart_toy</span><p></p>`;
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
     chatLi.innerHTML = chatContent;
     chatLi.querySelector("p").textContent = message;
     return chatLi;
@@ -29,7 +29,6 @@ const handleChat = () => {
     //append the user's message to the chatbox 
     chatbox.appendChild(createChatLi(userMessage, "outgoing"));
     chatbox.scrollTo(0, chatbox.scrollHeight);
-
 
     setTimeout(() => {
         // Display "Thinking..." message while waiting for the response
@@ -56,4 +55,4 @@ chatInput.addEventListener("input", () => {
   });
 sendChatBtn.addEventListener("click",handleChat);
 chatbotToggler.addEventListener("click", ()=> document.body.classList.toggle("show-chatbot"));
-chatbotCloseBtn.addEventListener("click", () => document.body.classList.remove("show-chatbo"));
+chatbotCloseBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
